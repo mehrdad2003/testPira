a697885451c306ebd506b206a82e65df3f794eab
unknown <mehrdadzare2003@gmail.com>
2022-05-22 18:46:49 +0430

    first

---
+++ routes/main.js
@@ -0,0 +1,7 @@
+const router=require('express').Router()
+const mainCtrl=require('../controllers/main')
+router.get('/',mainCtrl.getPosts)
+router.get('/mainSearch',mainCtrl.mainSearch)
+router.get('/detailPost/:id',mainCtrl.detailPost)
+
+module.exports=router
